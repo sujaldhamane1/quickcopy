8fe58b4a5ba8e8f5a4b051130a41e0ad888acc80
sujaldhamane1 <sujaldhamane1@gmail.com>
2025-03-26 00:11:05 +0530

    refactor: remove unused SLIPS_DIR constant and update file path in app.js; delete vercel.json configuration

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import clipboardy from 'clipboardy';
 
 const app = express();
 const PORT = 3000;
-const SLIPS_DIR = path.join('./', 'slips');
 
 app.get('/', (req, res) => {
     res.send('Welcome Bhaiji 😢!');
@@ -14,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/:slipid', (req, res) => {
     const slipid = req.params.slipid;
-    const sourceFile = path.join(SLIPS_DIR, `${slipid}.txt`);
+    const sourceFile = path.join('./slips', `${slipid}.txt`);
 
     fs.readFile(sourceFile, 'utf8', (err, data) => {
         if (err) {
